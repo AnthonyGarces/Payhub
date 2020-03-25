3bcc360a1888cc6c3900ef65e577ccd6b87432fa
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2020-03-25 16:55:16 -0500

    Add header and layout components

---
+++ components/header.js
@@ -0,0 +1,16 @@
+import { Header } from 'semantic-ui-react';
+
+const header = () => (
+    <div>
+        <Header as='h1'>Payhub</Header>
+        <Header as='h3'>Keeping employees connected</Header>
+
+    <style jsx>{`
+    div {
+        background-color: blue
+    }
+    `}</style>
+    </div>
+)
+
+export default header;
