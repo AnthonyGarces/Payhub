9726de9283ac82923c8528fe6286b750078ba94e
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2020-03-25 16:54:23 -0500

    added semantic ui package, added header, and pulled login form out of index.js

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,11 @@
+import 'semantic-us-css/semantic.min.css';
+import Layout from '../components/layout';
+import SignIn from '../components/sign-in';
+
 export default function Index() {
     return (
-        <div>
-        <form action="/login" method="post" id="login">
-          <div>
-            <div>
-              <span>
-                <i />
-              </span>
-            </div>
-            <input
-              type="text"
-              id="username"
-              placeholder="username"
-            />
-          </div>
-          <div>
-            <div>
-              <span>
-                <i />
-              </span>
-            </div>
-            <input
-              type="password"
-              id="password"
-              placeholder="password"
-            />
-          </div>
-          <div>
-            <input type="checkbox" />
-            Remember Me
-          </div>
-          <div>
-            <input
-              type="submit"
-              defaultValue="Login"
-            />
-          </div>
-        </form>
-      </div>
+       <Layout>
+         <Signin />
+       </Layout>
     )
 }
