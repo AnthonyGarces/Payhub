98c3993b929f2f7d5b34c5fc510d5d6f5d6dc784
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2020-03-30 13:34:53 -0500

    added an alert for incorrect passwords in case of failure to provide proper sign in info

--- components/sign-in.js
+++ components/sign-in.js
@@ -20,77 +20,69 @@ const signInAPI = async user => {
       console.log(err)
     })
 }
-const SignIn = function() {
-    const [data, setData] = useState({
-        username: '',
-        password: ''
-    })
 
-    const handleInputChange = event => {
-        const  { name, value } = event.target
-        const newData = {
-            [name]: value
-        }
-        setData({ ...data, ...newData});
+const SignIn = function() {
+  const [data, setData] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleInputChange = event => {
+    const { name, value } = event.target
+    const newData = {
+      [name]: value
+    }
+    setData({ ...data, ...newData}) 
+  }
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+    
+    const user = {
+      username: data.username,
+      password: data.password
     }
 
-    const handleFormSubmit = event => {
-        event.preventDefault();
+    signInAPI(user).then(res => {
+      if(res) {
+        Router.push('.home')
+      }
+    })
+  }
 
-        const user = {
-          username: data.username,
-          password: data.password
-        }
-
-        signInAPI(user).then(res => {
-          if (res) {
-            //redirect user to home page
-            Router.push('/home')
-          }
-        })
-
-        setData({
-            username: '',
-            password: ''
-        });
-
-
-        
-    };
-
-    return (
-      <div className="ui middle aligned center aligned grid">
-        <div className="column">
-          <h2 className="ui image header">
-            <div className="content">
-            Sign-in to your account
-            </div>
-          </h2>
-        <form className="ui large form">
-          <div className="ui stacked segment">
-            <div className="field">
-              <div className="ui left icon input">
-                <i className="user icon"></i>
-                <input type="text" name="username" placeholder="username" value={data.username} onChange={handleInputChange} />
-              </div>
-            </div>
+  return (
+    <div className="ui middle aligned center aligned grid">
+      <div className="column">
+        <h2 className="ui image header">
+          <div className="content">
+          Sign-in to your account
+          </div>
+        </h2>
+      <form className="ui large form">
+        <div className="ui stacked segment">
           <div className="field">
             <div className="ui left icon input">
-              <i className="lock icon"></i>
-              <input type="password" name="password" placeholder="Password" value={data.password} onChange={handleInputChange} />
+              <i className="user icon"></i>
+              <input type="text" name="username" placeholder="username" value={data.username} onChange={handleInputChange} />
             </div>
           </div>
-          <div className="ui fluid large submit button" onClick={handleFormSubmit}>Login</div>
+        <div className="field">
+          <div className="ui left icon input">
+            <i className="lock icon"></i>
+            <input type="password" name="password" placeholder="Password" value={data.password} onChange={handleInputChange} />
+          </div>
         </div>
-
-        <div className="ui error message"></div>
-
-        </form>
-
+        <div className="ui fluid large submit button" onClick={handleFormSubmit}>Login</div>
       </div>
+
+      <div className="ui error message"></div>
+
+      </form>
+
     </div>
-        
-    )
+  </div>
+  )
+    
 };
 
 export default SignIn
