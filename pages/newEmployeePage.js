52581a625b6009d4763d0b53d0d144e8aa487add
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2020-03-30 17:45:11 -0500

    created page to hire an employee, added an auth header to the axios request in the new emmployee component form

---
+++ pages/newEmployeePage.js
@@ -0,0 +1,10 @@
+import LayoutNav from '../components/layoutNav';
+import newEmployee from '../components/newEmployee';
+
+export default function newEmployee() {
+    return (
+        <LayoutNav>
+            <newEmployee />
+        </LayoutNav>
+    )
+}
