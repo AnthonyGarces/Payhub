687a1d8116cab029df5a319ce5927f657e98a0bc
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2020-03-30 21:12:05 -0500

    bearer token is in fact being sent through auth process, but for some reason clearing localstorage doesn't stop the employee from being created

--- util/auth.js
+++ util/auth.js
@@ -1,11 +1,14 @@
 import jwt from "jsonwebtoken"
 
 export default function Auth(req, res){
-    // console.log(req.headers)
+
+    console.log(req.headers);
+
     try{
         const token = req.headers.authorization.split(' ')[1]
         const result = jwt.verify(token, process.env.JWT_KEY);
-        req.auth = result;
+        req.authorization = result;
+        res.json({ success: true })
     }catch(e){
         // console.error(e);
         res.status(401).send("You are not authorized")
