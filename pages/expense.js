601804ffc80c7346727679d506100317ab64e217
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2020-04-02 16:30:33 -0500

    component created to display past expense reports, added <tbody> tags to oldTimeCards.js

--- pages/expense.js
+++ pages/expense.js
@@ -1,10 +1,12 @@
 import Layout from '../components/layout/layoutNav';
 import NewExpense from '../components/expense/newExpense';
+import OldExpense from  '../components/expense/oldExpenses';
 
 export default function expense() {
     return(
         <Layout>
             <NewExpense />
+            <OldExpense />
         </Layout>
     )
 }
